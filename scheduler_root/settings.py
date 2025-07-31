9c93813c50bf8080cbf3744ab9087ac99b3d2a6b
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2025-07-30 22:11:56 -0300

    chore: adiciona apps ao arquivo settings
    também adiciona novas rotas ao arquivo scheduler_root/urls

--- scheduler_root/settings.py
+++ scheduler_root/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'scheduler',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.CustomUser'
+
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend', 'accounts.backends.EmailBackend']
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
