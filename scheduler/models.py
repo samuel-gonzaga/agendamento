1387258fcb7c6ececdab643293e776c17a0bdcc0
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2025-08-04 09:43:46 -0300

    feat: cria model de agendamentos

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Scheduler(models.Model):
+    user_id = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='schedulers')
+    client_name = models.CharField(max_length=100)
+    service = models.CharField(max_length=100)
+    date = models.DateField()
+    time = models.TimeField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    observation = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.client_name
+
+    class Meta:
+        verbose_name = 'Scheduler'
+        verbose_name_plural = 'Schedulers'
+        ordering = ['-created_at']
