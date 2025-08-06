e74fb2e3b35a1916114ac7fb0df0e4859b4c3b81
Samuel Henricky <samuel.gonzaga.dev@gmail.com>
2025-08-05 22:08:02 -0300

    feat: refatora model Scheduler para Apointment
    Renomeia a model do Scheduler para Appointment para refletir melhor sua proposta.
    
    Adiciona um campo de status para rastrear o estado do agendamento (pendente, confirmado, cancelado).
    Inclui um campo de número de telefone.
    Melhora a representação em string do modelo para melhor legibilidade.
    Adiciona uma restrição de unicidade para evitar agendamentos duplicados para o mesmo usuário, data e horário.

--- scheduler/models.py
+++ scheduler/models.py
@@ -1,20 +1,26 @@
 from django.db import models
 
-class Scheduler(models.Model):
-    user_id = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='schedulers')
+class Appointment(models.Model):
+
+    STATUS_CHOICES = [
+        ('pending', 'Pendente'),
+        ('confirmed', 'Confirmado'),
+        ('canceled', 'Cancelado'),
+    ]
+
+    user_id = models.ForeignKey('accounts.CustomUser', on_delete=models.CASCADE, related_name='appointments')
     client_name = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=11, blank=True, null=True)
     service = models.CharField(max_length=100)
     date = models.DateField()
     time = models.TimeField()
+    status = models.CharField(max_length=50, choices=STATUS_CHOICES, default='pending')
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    observation = models.TextField(blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.client_name
+        return f"{self.service} com {self.client_name} em {self.date} às {self.time}"
 
     class Meta:
-        verbose_name = 'Scheduler'
-        verbose_name_plural = 'Schedulers'
-        ordering = ['-created_at']
+        unique_together = ('user_id', 'date', 'time')
